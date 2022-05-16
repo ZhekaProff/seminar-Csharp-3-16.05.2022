ab69b0d659fbb5ad7e56d9041f839e2f57d5e5c1
ZhekaProff <Zhekakad@ya.ru>
2022-05-16 16:29:47 +0900

    Показать таблицу квадратов чисел от 1 до N

---
+++ lesson01/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <=n; i++)
+{
+    Console.Write($"{i * i} ");
+}
